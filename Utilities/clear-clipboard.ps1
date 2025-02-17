36edac702f89395e88bbe1bb9c9e20b7ee64b89a
LoneStarCoder <133882997+LoneStarCoder@users.noreply.github.com>
2025-02-17 16:54:19 -0600

    Update clear-clipboard.ps1

--- Utilities/clear-clipboard.ps1
+++ Utilities/clear-clipboard.ps1
@@ -2,6 +2,7 @@ while ($true) {
  if (Get-Clipboard) {
   Write-host "Clip board has stuff, clearing in 3 seconds."
   start-sleep -Seconds 3
-  set-clipboard -Value $null; write-host "cleared"
+  set-clipboard -Value $null
+  write-host "cleared"
   }
 }
